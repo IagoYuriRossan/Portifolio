133fd6bfe18d8ba32340cc8da8a970361404e710
IagoYuriRossan <iagorossan321@gmail.com>
2026-02-24 20:48:08 -0300

    feat: adicionar suporte ao react-native-safe-area-context para melhor gerenciamento de áreas seguras

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import { ScrollView, View, Text, SafeAreaView } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider, useTheme } from './src/context/ThemeContext';
 import { getStyles } from './src/components/themedStyles';
